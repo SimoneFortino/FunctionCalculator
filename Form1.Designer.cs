36301e39e05005da34197a32b73bc37875376994
lucaciao <luca@banfi.me>
2025-12-04 22:15:27 +0100

    Deleted Button1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -111,16 +111,6 @@ namespace FunctionCalculator
             this.panel1.Size = new System.Drawing.Size(566, 219);
             this.panel1.TabIndex = 7;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 137);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 22);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
