23dd2b57a2e5b6098ff85ae77c9894bb69d823d5
SimoneFortino <merkurio.golden@gmail.com>
2025-12-04 21:44:23 +0100

    funziona 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,6 +37,7 @@ namespace FunctionCalculator
             this.resultLabel = new System.Windows.Forms.Label();
             this.resultTitelLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // calculateButton
@@ -110,11 +111,22 @@ namespace FunctionCalculator
             this.panel1.Size = new System.Drawing.Size(566, 219);
             this.panel1.TabIndex = 7;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(615, 451);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.resultTitelLabel);
             this.Controls.Add(this.resultLabel);
@@ -128,6 +140,8 @@ namespace FunctionCalculator
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button1;
 
         private System.Windows.Forms.Panel panel1;
 
--- Form1.cs
+++ Form1.cs
@@ -3,28 +3,41 @@ using System.Windows.Forms;
 using System.Drawing;
 using ScottPlot;
 using ScottPlot.WinForms;
+using NCalc;
+using ScottPlot.DataSources;
 
 namespace FunctionCalculator
 {
     public partial class Form1 : Form
     {
         
-        readonly FormsPlot FormsPlot1 = new FormsPlot() { Dock = DockStyle.Fill };
+        // readonly FormsPlot formsPlot = new FormsPlot() { Dock = DockStyle.Fill };
+        private FormsPlot plot = new FormsPlot() { Dock = DockStyle.Fill };
+        
+        
         
         public Form1()
         {
             InitializeComponent();
             
             // Add the FormsPlot to the panel
-            panel1.Controls.Add(FormsPlot1);
-
-            // Plot data using the control
-            double[] data = ScottPlot.Generate.Sin();
-            FormsPlot1.Plot.Add.Signal(data);
+            panel1.Controls.Add(plot);
+            
+            
+            
+            
+            /*
             var cross = FormsPlot1.Plot.Add.Crosshair(0, 0);
+            
             cross.HorizontalLine.Color = new ScottPlot.Color(System.Drawing.Color.Black);
+            cross.HorizontalLine.LineWidth = 1;
+            
             cross.VerticalLine.Color = new ScottPlot.Color(System.Drawing.Color.Black);
-            FormsPlot1.Refresh();
+            cross.VerticalLine.LineWidth = 1;
+            */
+            
+            
+            
             
             
             
@@ -37,6 +50,18 @@ namespace FunctionCalculator
             {
                 double parameterValue = Convert.ToDouble(parameterTextBox.Text);
                 resultLabel.Text = Convert.ToString(calculator.ParseFunction(functionTextBox.Text, parameterValue));
+
+                var expr = new Expression(functionTextBox.Text);
+
+                // aggiunge la funzione usando NCalc
+                plot.Plot.Add.Function(x =>
+                {
+                    expr.Parameters["x"] = x;
+                    return Convert.ToDouble(expr.Evaluate());
+                });
+                plot.Plot.Axes.SetLimits(-5, 5, -5, 5);
+                plot.Refresh();
+                
             }
             catch (Exception exception)
             {
@@ -48,6 +73,11 @@ namespace FunctionCalculator
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
